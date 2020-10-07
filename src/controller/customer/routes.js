f9f6680f1dd673ffa59e5d51b2ae9ebfb27ea842
unknown <baykalaserdar@gmail.com>
2020-10-07 02:30:19 +0200

    auth activated

--- src/controller/customer/routes.js
+++ src/controller/customer/routes.js
@@ -91,7 +91,7 @@ const router = new express.Router()
  */
 //#endregion
 
-router.post("/", checkSchema(validations.create), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'create'))
+router.post("/", auth, checkSchema(validations.create), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'create'))
 
 //#region [swagger: /customers - GET]
 /**
@@ -129,7 +129,7 @@ router.post("/", checkSchema(validations.create), schemaErrorHandler(), controll
  *            
  */
 //#endregion
-router.get('/', checkSchema(validations.list), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'list'))
+router.get('/', auth, checkSchema(validations.list), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'list'))
 
 
 //#region [swagger: /customers/{id} - GET]
@@ -173,7 +173,7 @@ router.get('/', checkSchema(validations.list), schemaErrorHandler(), controllerA
  *         
  */
 //#endregion
-router.get('/:id', checkSchema(validations.get), schemaErrorHandler(),controllerAdapter(customerControllerInstance, 'get'))
+router.get('/:id', auth, checkSchema(validations.get), schemaErrorHandler(),controllerAdapter(customerControllerInstance, 'get'))
 
 
 //#region [swagger: /customers/{id} - PATCH]
@@ -233,7 +233,7 @@ router.get('/:id', checkSchema(validations.get), schemaErrorHandler(),controller
  *         
  */
 //#endregion
-router.patch('/:id', checkSchema(validations.update), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'update'))
+router.patch('/:id', auth, checkSchema(validations.update), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'update'))
 
 
 //#region [swagger: /customers/{id} - DELETE]
@@ -277,7 +277,7 @@ router.patch('/:id', checkSchema(validations.update), schemaErrorHandler(), cont
  *
  */
 //#endregion
-router.delete('/:id', checkSchema(validations.delete), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'delete'))
+router.delete('/:id', auth, checkSchema(validations.delete), schemaErrorHandler(), controllerAdapter(customerControllerInstance, 'delete'))
 
 
 module.exports = router;
